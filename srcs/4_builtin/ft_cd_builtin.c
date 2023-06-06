f06caa010dbc90c8613fefc65ce54cd429c5aa2f
Taeil Nam (Tayden) <tnam@student.42seoul.kr>
2023-06-06 12:35:52 +0900

    Tnam/feature/exec (#20)
    * modify: cat 실행 중 시그널 처리 수정
    
    * feat: cd built-in 함수 추가
    
    Related to: #3
    
    * modify: envp list->char ** 변환 수정
    
    Related to: #3
    
    * modify: cd에 인자가 없는 경우 예외 처리 추가
    
    Related to: #3
    
    * modify: 없는 환경변수 치환 로직 수정
    
    Related to: #2
    
    * modify: echo 명령어 -n 옵션 적용시, 맨 공백문자 삭제하도록 수정
    
    - Related to: #3
    
    * modify: exit built-in 함수 인자 예외 처리시 exit(255)로 수정
    
    - Related to: #3
    
    * modify: exit built-in 함수 로직 수정
    
    - exit 3 처럼 한자리 수가 인자로 들어오면 예외 처리되던 것 수정.
    
    Related to: #3
    
    * modify: cd built-in 함수 에러시 exit_code 1로 수정
    
    Related to: #3
    
    * modify: syntax error시 exit_code = 258 추가
    
    Related to: #3
    
    * feat: echo 인자 없을 시, 개행 출력 후 종료되도록 수정
    
    Related to: #3
    
    * modify: env built-in 실행시, value가 없는 환경변수는 출력되지 않도록 수정
    
    Related to: #3
    
    * modify: envp 맨 마지막에 NULL 추가
    
    - envp 맨 마지막에 NULL로 끝나지 않으면 UB 발생.
    
    Related to: #3
    
    * modify: SIGINT 발생시, $? 결과 값 1로 변경
    
    Related to: #3
    
    * modify: 디렉토리를 대상으로 명령어 입력시 예외 처리 추가
    
    - ex) bash: /: is a directory
    
    Related to: #3
    
    * modify: 오류 메시지 통일화 및 direcroty 확인 전용 함수 분할
    
    Related to: #3
    
    * modify: built-in 함수들 자식 프로세스에서 실행하도록 수정
    
    - 부모 프로세스에서 실행해야 하는 경우 로직 추가 중
    
    Related to: #3
    
    * feat: built-in 함수 cd, export, unset, exit 단독 사용시, 부모 프로세스에서 실행
    
    - cd = 부모 프로세스의 현재 디렉토리 변경.
    - export = 부모 프로세스의 환경 변수 수정.
    - unset = 부모 프로세스의 환경 변수 삭제.
    - exit = 부모 프로세스에서 예외 발생시, exit 되지 않도록 수정.
    
    Related to: #3
    
    * modify: export 인자 없이 사용시, 자식 프로세스에서 실행되도록 수정
    
    Related to: #3
    
    * modify: cd 실패시, 에러 메세지 출력
    
    - Related to: #3

--- srcs/4_builtin/ft_cd_builtin.c
+++ srcs/4_builtin/ft_cd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:39:21 by tnam              #+#    #+#             */
-/*   Updated: 2023/06/02 09:40:02 by tnam             ###   ########.fr       */
+/*   Updated: 2023/06/06 10:42:24 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,21 @@ int	ft_cd_builtin(t_exec_info *exec_info)
 		if (exec_info->builtin_parent == TRUE)
 			return (SUCCESS);
 		else
-			exit(SUCCESS);
+			exit(EXIT_SUCCESS);
 	}
 	if (chdir(path) == FAILURE)
 	{
 		g_child_exit_code = 1;
 		if (exec_info->builtin_parent == TRUE)
-			return (SUCCESS);
+		{
+			ft_printf_err("cd: %s: ", path);
+			return (ft_perror(SUCCESS));
+		}
 		else
-			exit(SUCCESS);
+			exit(EXIT_SUCCESS);
 	}
 	if (exec_info->builtin_parent == TRUE)
 		return (SUCCESS);
 	else
-		exit(SUCCESS);
+		exit(EXIT_SUCCESS);
 }
