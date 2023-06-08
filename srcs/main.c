b44b2bcb432fddbe6df121064fd5fa311603b48e
Taeil-Nam <67255013+Taeil-Nam@users.noreply.github.com>
2023-06-08 18:11:23 +0900

    Style: 테스트용 코드 삭제

--- srcs/main.c
+++ srcs/main.c
@@ -12,56 +12,6 @@
 
 #include "minishell.h"
 
-/* print test function */
-void	print_tokens(t_parse *parse)
-{
-	char	*type;
-
-	printf("\n=========\n");
-	for (size_t i = 0; i < parse->token_count; i++)
-	{
-		if (parse->tokens[i].type == WORD)
-			type = "WORD";
-		if (parse->tokens[i].type == PIPE)
-			type = "PIPE";
-		if (parse->tokens[i].type == REDIRECT)
-			type = "REDIRECT";
-		printf("Token%zu type = %s\n", i + 1, type);
-		printf("Token%zu str = %s\n", i + 1, parse->tokens[i].str);
-		printf("---------\n");
-	}
-	printf("=========\n\n");
-}
-
-/* print test function */
-void	print_exec(t_exec *exec)
-{
-	printf("\n=========\n");
-	for (size_t i = 0; i < exec->exec_arr_size; i++)
-	{
-		printf("cmd_path = %s\n", exec->exec_arr[i].cmd_path);
-		for (size_t j = 0; exec->exec_arr[i].cmd[j] != NULL; j++)
-		{
-			printf("cmd[%zu] = %s\n", j, exec->exec_arr[i].cmd[j]);
-		}
-		printf("---------\n");
-		for (size_t j = 0; ; j++)
-		{
-			if (exec->exec_arr[i].redirect[j].value == 0)
-				break ;
-			printf("redirect[%zu].type = %u\n", j, exec->exec_arr[i].redirect[j].type);
-			printf("redirect[%zu].value = %s\n", j, exec->exec_arr[i].redirect[j].value);
-		}
-		printf("---------\n");
-	}
-	printf("=========\n\n");
-}
-
-void leaks() // memory leak test
-{
-	system("leaks -q $PPID");
-}
-
 static void	ft_parse_execute(t_info *info, t_parse *parse, t_exec *exec)
 {
 	if (ft_parse(info, parse) == FAILURE)
@@ -70,8 +20,6 @@ static void	ft_parse_execute(t_info *info, t_parse *parse, t_exec *exec)
 		return ;
 	if (ft_exec(info, parse, exec) == FAILURE)
 		return ;
-	//print_tokens(parse); // test
-	//print_exec(exec); // test
 	ft_free_all(parse, exec);
 }
 
@@ -81,7 +29,6 @@ int	main(int argc, char *argv[], char *envp[])
 	t_parse		parse;
 	t_exec		exec;
 
-	//atexit(leaks); // memory leaks test
 	ft_init(argc, argv, envp, &info);
 	while (TRUE)
 	{
